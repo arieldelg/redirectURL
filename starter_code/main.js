053945d6ec1b706010a2002a02103fd215b22af6
arieldelg <arieldelgrande@hotmail.com>
2025-03-21 22:57:08 -0700

    delete start code

--- starter_code/main.js
+++
@@ -1,8 +0,0 @@
-import "./style.css";
-
-const app = document.getElementById("app");
-const loginBtn = document.getElementById("btn-siwa");
-
-const loginSIWA = async () => {};
-
-loginBtn.addEventListener("click", loginSIWA);
